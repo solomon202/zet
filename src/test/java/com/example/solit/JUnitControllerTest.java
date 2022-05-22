212b47b9fa06e604660e85d165a8e43ea6d56097
vasy <solomon202.pi@yandex.ru>
2022-05-22 21:50:23 +0300

    three

---
+++ src/test/java/com/example/solit/JUnitControllerTest.java
@@ -0,0 +1,33 @@
+package com.example.solit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//@ContextConfiguration(classes = ElastSearchBootApplication.class)
+import com.example.solit.controller.AuthController;
+import com.example.solit.controller.HomeController;
+//@RunWith необходима для включения тестирования интеграции Spring
+//@RunWith (SpringRunner.class) обеспечивает мост между функциями тестирования Spring Boot и JUnit. Эта аннотация потребуется всякий раз, когда мы используем какие-либо функции тестирования 
+//Spring Boot в наших тестах JUnit.
+@RunWith(SpringRunner.class)
+//анотацыя Чтобы протестировать компонент / компонент в приложении Spring Boot,
+//@SpringBootTest нужен чтобы загрузит полный контекст приложения,и протестировать любойбин 
+//обезательно название тестого  пакета должно быть такимже как и приложения !!!!!
+@SpringBootTest
+public class JUnitControllerTest {
+	 @Autowired
+	public  HomeController  homeController;
+//@Test — определение данного метода в качестве тестируемого (по сути — метод, помеченный данной аннотацией и есть модульный тест).
+    @Test
+    public void testHomeController() {
+    	//создать обьект без запуска сприга если хотим запустить одельно 
+   	//HomeController  homeController = new HomeController();
+        String result = homeController.home();
+        assertEquals(result, "Hello World!");
+    }
+}
--- src/test/java/com/example/solit/MainControllerTest.java
+++ src/test/java/com/example/solit/MainControllerTest.java
@@ -1,4 +1,4 @@
-package com.example.demorepo;
+package com.example.solit;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,6 +18,7 @@ import static org.springframework.http.MediaType.TEXT_PLAIN;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 //позволяет подключить спринг в тесты. 
 @ExtendWith(SpringExtension.class)
 //подключение к проверяемым на тест классам 
@@ -27,51 +28,49 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebAppConfiguration
 public class MainControllerTest {
 //Бин — создаваемый Spring-ом объект класса, который можно внедрить в качестве значения поля в другой объект.
-	//Эта операция называется «инжектнуть» (inject).внедрить
-    @Autowired
-    private  EmployeeController  employeeController;
-    
-    
-    @Autowired
-	private  AuthController  authController;
+	// Эта операция называется «инжектнуть» (inject).внедрить
+	@Autowired
+	private EmployeeController employeeController;
+
+	@Autowired
+	private AuthController authController;
 
 //через него делаем запросы к контроллерам 
-    private MockMvc mvc;
+	private MockMvc mvc;
+
 //Аннотацией @BeforeEach помечаются методы, которые будут выполняться перед стартом каждого из тестовых методов.
-    //делаем какуюто подготовку 
-    @BeforeEach
-    //Это говорит Spring, чтобы внедрить WebApplicationContext он создал ранее, в ваш тест. Затем это используется в очень простом однострочном методе setup() :
-    public void setup() {
-        mvc = MockMvcBuilders.webAppContextSetup((WebApplicationContext) employeeController).build();
-    }
+	// делаем какуюто подготовку
+	@BeforeEach
+	// Это говорит Spring, чтобы внедрить WebApplicationContext он создал ранее, в
+	// ваш тест. Затем это используется в очень простом однострочном методе setup()
+	// :
+	public void setup() {
+		mvc = MockMvcBuilders.webAppContextSetup((WebApplicationContext) employeeController).build();
+	}
+
 //Аннотация @Test используется для указания того, что аннотированный метод является методом тестирования.
 //@Методы тестирования не должны быть частными или статическими. Методы @Test не должны возвращать значение.
 //@Методы тестирования могут при необходимости объявлять параметры, которые должны быть разрешены с помощью решателей параметров.
-    @Test
-    public void homeShouldRedirectToUsersPage() throws Exception {
-        mvc.perform(get("/shop")).andDo(print())
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/sushi"));
-    }
-    // MockMvc состоит из 3 шагов: построение мок-объекта, 
-    //отправка HTTP-запроса контроллеру и собственно анализ результатов.
+	@Test
+	public void homeShouldRedirectToUsersPage() throws Exception {
+		mvc.perform(get("/shop")).andDo(print()).andExpect(status().is3xxRedirection())
+				.andExpect(redirectedUrl("/sushi"));
+	}
+	// MockMvc состоит из 3 шагов: построение мок-объекта,
+	// отправка HTTP-запроса контроллеру и собственно анализ результатов.
 
-    @Test
-    public void viewShouldReturnGreetingWithSpecifiedName() throws Exception {
-        String name = "stranger";
-        String expected = "Привет " + name;
+	@Test
+	public void viewShouldReturnGreetingWithSpecifiedName() throws Exception {
+		String name = "stranger";
+		String expected = "Привет " + name;
 
-        mvc.perform(get("/view/{name}", name))
-                .andExpect(status().isOk())
-                .andExpect(model().attribute("msg", expected))
-                .andExpect(view().name("/index"));
-    }
+		mvc.perform(get("/view/{name}", name)).andExpect(status().isOk()).andExpect(model().attribute("msg", expected))
+				.andExpect(view().name("/index"));
+	}
 
-    @Test
-    public void rawShouldReturnResponseBody() throws Exception {
-        mvc.perform(get("/raw"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(TEXT_PLAIN))
-                .andExpect(content().string("Raw data"));
-    }
+	@Test
+	public void rawShouldReturnResponseBody() throws Exception {
+		mvc.perform(get("/raw")).andExpect(status().isOk()).andExpect(content().contentTypeCompatibleWith(TEXT_PLAIN))
+				.andExpect(content().string("Raw data"));
+	}
 }
